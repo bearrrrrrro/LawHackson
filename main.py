d67eb9d162cb55d19c4d205951262dbad19e1302
bear <bear890707@gmail.com>
2021-08-14 16:12:15 +0800

    now output 2 csv files

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import sys
 import logging
 
-from glob import glob
 from pathlib import Path
 from time import time
 
@@ -18,14 +17,23 @@ import location
 # import Get_Address
 
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='main.log', level=logging.DEBUG)
-    start = time()
+def _output_money(df: pd.DataFrame, filename: str) -> None:
+    result = df.drop(columns=['detention'])
+    result[result['money'] > 0].to_csv(
+        f'{filename}_with_money.csv', index=True, encoding='utf-8')
 
-    csv_data = []
-    pattern = str(Path(__file__).parent.joinpath('input').joinpath('*.json'))
+
+def _output_detention(df: pd.DataFrame, filename: str) -> None:
+    result = df.drop(columns=['money'])
+    result[result['detention'] > 0].to_csv(
+        f'{filename}_with_detention.csv', index=True, encoding='utf-8')
+
+
+def _run(judge_path):
+    print(f'Processing {judge_path.name} ...', file=sys.stderr)
     error_count = 0
-    for filename in tqdm(glob(pattern)):
+    csv_data = []
+    for filename in tqdm(list(judge_path.iterdir())):
         with open(filename, 'r', encoding="utf-8") as f:
             data = json.load(f)
 
@@ -46,10 +54,17 @@ if __name__ == '__main__':
                 csv_data.append(row_data)
         except:
             logging.error(data, exc_info=True)
-            print("Error:", filename, file=sys.stderr)
             error_count += 1
 
-
-    # print(csv_data)
-    pd.DataFrame.from_records(data=csv_data).to_csv('result.csv', index=True, encoding='utf-8')
+    result = pd.DataFrame.from_records(data=csv_data)
+    _output_money(result, judge_path.name)
+    _output_detention(result, judge_path.name)
     print(f"Time used: {time() - start}, error: {error_count}", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='main.log', level=logging.DEBUG)
+    start = time()
+
+    for judge_path in Path(__file__).parent.joinpath('json').iterdir():
+        _run(judge_path)
